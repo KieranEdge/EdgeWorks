38d27304a7e873f417e3ff6b9c62e33db0a0618e
Kieran Edge <kesedge94@gmail.com>
2021-12-15 16:42:26 +0000

    Auto sizing window function added

--- src/main/java/GUI/MainWindow.java
+++ src/main/java/GUI/MainWindow.java
@@ -1,4 +1,31 @@
 package GUI;
 
+
+// Importing the GUI libraries
+import javax.swing.*;
+
+// Class containing the Main Window of the GUI
 public class MainWindow {
+
+    // Class Constructor
+    public MainWindow(){
+    }
+
+    public static void main(String args[]){
+        // Creating the frame
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Sizing the window
+        int[] size = WindowFunctions.winSize();
+        System.out.println("Window size: " + size[0] + " x " + size[1]);
+        frame.setSize(size[0], size[1]);
+
+        // Adding a button
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+
+        // Making the frame show
+        frame.setVisible(true);
+    }
 }
