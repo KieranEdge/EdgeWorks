5de27f453d81539b979037c75eca2f9fd4e629ef
Kieran Edge <kesedge94@gmail.com>
2021-12-15 17:11:09 +0000

    Change windows name

--- src/main/java/Vision/Camera.java
+++ src/main/java/Vision/Camera.java
@@ -1,5 +1,8 @@
 package Vision;
+import org.opencv.core.Core;
 
 public class Camera{
-
+    public void version(){
+        System.out.println(Core.VERSION);
+    }
 }
--- src/main/java/Vision/VisTest.java
+++ src/main/java/Vision/VisTest.java
@@ -3,6 +3,7 @@ import org.opencv.core.Core;
 
 public class VisTest {
     public static void main(String[] args) {
-        System.out.println(Core.VERSION);
+        Camera cam = new Camera();
+        cam.version;
     }
 }
