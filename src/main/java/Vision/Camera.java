effc5b1e6f07196a834c84ca9bf0371f97f60407
Kieran Edge <kesedge94@gmail.com>
2021-12-15 17:30:25 +0000

    Get opencv version

--- src/main/java/Vision/Camera.java
+++ src/main/java/Vision/Camera.java
@@ -1,8 +1,29 @@
 package Vision;
 import org.opencv.core.Core;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.github.sarxos.webcam.Webcam;
+
 public class Camera{
     public static void version(){
         System.out.println(Core.VERSION);
+    }
+
+    public static void captureSingle() throws IOException{
+        // get default webcam and open it
+        Webcam webcam = Webcam.getDefault();
+        webcam.open();
+
+        // get image
+        BufferedImage image = webcam.getImage();
+
+        // save image to PNG file
+        ImageIO.write(image, "PNG", new File("test.png"));
+
     }
 }
--- src/main/java/Vision/VisTest.java
+++ src/main/java/Vision/VisTest.java
@@ -1,8 +1,11 @@
 package Vision;
 import org.opencv.core.Core;
 
+import java.io.IOException;
+
 public class VisTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Camera.version();
+        Camera.captureSingle();
     }
 }
