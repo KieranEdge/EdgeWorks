cf995477ddcdbd183c5c3d6d58038abf69a4a5c4
Kieran Edge <kesedge94@gmail.com>
2021-12-16 13:41:57 +0000

    Creating GUI class attributes

--- src/main/java/GUI/MainWindow.java
+++ src/main/java/GUI/MainWindow.java
@@ -6,30 +6,34 @@ import javax.swing.*;
 
 // Class containing the Main Window of the GUI
 public class MainWindow {
+    // Creating the class attributes
+    JFrame mainFrame;
+    JFrame imageFrame;
+    JFrame outputFrame;
 
     // Class Constructor
     public MainWindow(){
         // Creating  the frames for the GUI
-        JFrame mainFrame = new JFrame();
-        JFrame imageFrame = new JFrame();
-        JFrame outputFrame = new JFrame();
+        mainFrame = new JFrame("EdgeWare");
+        imageFrame = new JFrame();
+        outputFrame = new JFrame();
     }
 
     public static void main(String args[]){
         // Creating the frame
-        JFrame frame = new JFrame("Edgeware");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        MainWindow Frames = new MainWindow();
+        Frames.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Sizing the window
         int[] size = WindowFunctions.winSize();
         System.out.println("Dimensions: " + size[0] + " x " + size[1]);
-        frame.setSize(size[0], size[1]);
+        Frames.mainFrame.setSize(size[0], size[1]);
 
         // Adding a button
         JButton button = new JButton("Press");
-        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        Frames.mainFrame.getContentPane().add(button); // Adds Button to content pane of frame
 
         // Making the frame show
-        frame.setVisible(true);
+        Frames.mainFrame.setVisible(true);
     }
 }
