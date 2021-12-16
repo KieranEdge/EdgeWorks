8a357425253a55168060c5162bdc666a8a9eac25
Kieran Edge <kesedge94@gmail.com>
2021-12-16 11:17:46 +0000

    - Removed OpenCV Version - Added documentation

--- src/main/java/Vision/Camera.java
+++ src/main/java/Vision/Camera.java
@@ -1,19 +1,27 @@
 package Vision;
-import org.opencv.core.Core;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-
 import com.github.sarxos.webcam.Webcam;
 
-public class Camera{
-    public static void version(){
-        System.out.println(Core.VERSION);
-    }
 
+/**
+ *
+ The purpose of this class is to access and control the internal webcam of a computer
+ *
+ * @version
+1.00 16 Dec 2021
+ * @author
+Kieran Edge
+ */
+
+public class Camera{
+
+    /*
+    The method below accesses the default webcam and captures a single image and saves it
+     */
     public static void captureSingle() throws IOException{
         // get default webcam and open it
         Webcam webcam = Webcam.getDefault();
--- src/main/java/Vision/VisTest.java
+++ src/main/java/Vision/VisTest.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 
 public class VisTest {
     public static void main(String[] args) throws IOException {
-        Camera.version();
         Camera.captureSingle();
     }
 }
